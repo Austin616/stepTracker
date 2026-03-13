fdbebefc4e5dbcaf5f68d74f87575c9a3447bf6f
austin616 <austintran616@gmail.com>
2026-03-13 16:53:47 -0500

    inoit

---
+++ stepTracker/stepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepTrackerApp.swift
+//  stepTracker
+//
+//  Created by Austin Tran on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct stepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stepTrackerTests/stepTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  stepTrackerTests.swift
+//  stepTrackerTests
+//
+//  Created by Austin Tran on 3/13/26.
+//
+
+import Testing
+@testable import stepTracker
+
+struct stepTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
